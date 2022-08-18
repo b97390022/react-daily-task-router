3015bf97ef622dfef26df07bbcae0a75552963d7
BOSHAU CHEN <b97390022@gmail.com>
2022-08-19 00:36:24 +0800

    feat: adding three routes(Register, Login, Todo)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {
   HashRouter,
   NavLink,
+  Routes,
+  Route
 } from 'react-router-dom';
 
 const Todo = () => {
@@ -17,26 +19,31 @@ const Register = () => {
 
 function App() {
   return (
-    <div className="container">
-      <HashRouter>
-        <div className="nav-link">
-          <NavLink to="/">
-            <p>回到首頁</p>
-          </NavLink>
-          <NavLink to="/register">
-            <p>註冊頁面</p>
-          </NavLink>
-          <NavLink to="/login">
-            <p>登入頁面</p>
-          </NavLink>
-          <NavLink to="/todo">
-            <p>Todo 頁面</p>
-          </NavLink>
-        </div>
-        {/* Routes, Route 練習區 */}
-        {/* 練習區 */}
-      </HashRouter>
-    </div>
+      <div className="container">
+          <HashRouter>
+              <div className="nav-link">
+                  <NavLink to="/">
+                      <p>回到首頁</p>
+                  </NavLink>
+                  <NavLink to="/register">
+                      <p>註冊頁面</p>
+                  </NavLink>
+                  <NavLink to="/login">
+                      <p>登入頁面</p>
+                  </NavLink>
+                  <NavLink to="/todo">
+                      <p>Todo 頁面</p>
+                  </NavLink>
+              </div>
+              {/* Routes, Route 練習區 */}
+              <Routes>
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/todo" element={<Todo />} />
+              </Routes>
+              {/* 練習區 */}
+          </HashRouter>
+      </div>
   );
 }
 
